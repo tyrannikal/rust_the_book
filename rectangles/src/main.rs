2c9cf1f3bd93a07842685fdc4c966535f554d828
tyrannikal <101421596+tyrannikal@users.noreply.github.com>
2026-02-26 20:18:20 -0500

    added more structs and methods practice code

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,3 +1,66 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn width(&self) -> bool {
+        self.width > 0
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width >= other.width && self.height >= other.height
+    }
+
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let scale = 2;
+    let rect = Rectangle {
+        width: dbg!(30 * scale),
+        height: 50,
+    };
+
+    dbg!(&rect);
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect.area()
+    );
+
+    println!("rect is {rect:#?}");
+
+    println!("rectangle has a nonzero width of {}", rect.width);
+    println!("rectangle has a nonzero width of {}", rect.width());
+
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    let rect4 = Rectangle::square(15);
+
+    dbg!(rect4);
 }

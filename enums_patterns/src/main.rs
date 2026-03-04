17c0f6ae340635c51267d93bf313c3ce1312e90f
tyrannikal <101421596+tyrannikal@users.noreply.github.com>
2026-03-03 20:45:14 -0500

    added to enums_patterns examples

--- enums_patterns/src/main.rs
+++ enums_patterns/src/main.rs
@@ -23,6 +23,25 @@ impl Message {
     }
 }
 
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky Penny!");
+            1
+        }
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
+
 fn main() {
     let home = dbg!(IpAddr::V4(String::from("127.0.0.1")));
     let loopback = dbg!(IpAddr::V6(String::from("::1")));
@@ -39,13 +58,19 @@ fn main() {
 
     let m1 = Message::Write(String::from("hello"));
     m1.call();
-
     let m2 = Message::Move { x: 2, y: 4 };
     m2.call();
-
     let m3 = Message::ChangeColor(2, 4, 8);
     m3.call();
-
     let m4 = Message::Quit;
     m4.call();
+
+    let coin1 = Coin::Penny;
+    value_in_cents(coin1);
+    let coin2 = Coin::Nickel;
+    value_in_cents(coin2);
+    let coin3 = Coin::Dime;
+    value_in_cents(coin3);
+    let coin4 = Coin::Quarter;
+    value_in_cents(coin4);
 }
